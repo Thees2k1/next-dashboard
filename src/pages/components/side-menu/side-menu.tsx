4f9d8aeff746a51e5942778f8f2f053acf30dc0f
Thees2k1 <tongnguyenthe@gmail.com>
2024-06-24 05:47:47 +0700

    added project structure files

---
+++ src/pages/components/side-menu/side-menu.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SideMenu = () => {
+  return (
+    <ul>
+        <li>Analytic</li>
+        <li>Profile</li>
+        <li>Setting</li>
+        <li>Others</li>
+    </ul>
+  )
+}
+
+export default SideMenu
